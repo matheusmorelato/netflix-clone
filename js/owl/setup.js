2b7d18a088613c2475716266f0efd2f65a4e75ea
Matheus Morelato <44590657+matheusmorelato@users.noreply.github.com>
2021-06-28 08:58:43 -0300

    Add files via upload

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:7
+        }
+    }
+})
